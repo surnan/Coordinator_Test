d226bb37aa16a9403f9c78deb343fe45d423d491
Suresh N <forsuresh@yandex.com>
2019-07-10 21:38:16 -0400

    added comments

--- Coordinator Test/Coordinator Test/Coordinators/AnotherBuyCoordinator.swift
+++ Coordinator Test/Coordinator Test/Coordinators/AnotherBuyCoordinator.swift
@@ -9,19 +9,19 @@
 import UIKit
 
 
-class AnotherBuyCoordinator: Coordinator {
-    weak var parentCoordinator: MainCoordinator?
-    
-    var childCoordinators = [Coordinator]()
-    var navigationController: UINavigationController
-    
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
-    
-    func start(){
-        let vc = AnotherBuyVC()
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: true)
-    }
-}
+//class AnotherBuyCoordinator: Coordinator {
+//    weak var parentCoordinator: MainCoordinator?
+//    
+//    var childCoordinators = [Coordinator]()
+//    var navigationController: UINavigationController
+//    
+//    init(navigationController: UINavigationController) {
+//        self.navigationController = navigationController
+//    }
+//    
+//    func start(){
+//        let vc = AnotherBuyVC()
+//        vc.coordinator = self
+//        navigationController.pushViewController(vc, animated: true)
+//    }
+//}
--- Coordinator Test/Coordinator Test/Model/BuyViewControllerCoordinator.swift
+++ Coordinator Test/Coordinator Test/Model/BuyViewControllerCoordinator.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class BuyViewControllerCoordinator: Coordinator {
-    weak var parentCoordinator: Coordinator?
-    var childCoordinators = [Coordinator]()
-    var navigationController: UINavigationController
-    
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
-    
-    func start() {
-        let vc = BuyViewController.instantiate()
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: false)
-    }
-}
+//class BuyViewControllerCoordinator: Coordinator {
+//    weak var parentCoordinator: Coordinator?
+//    var childCoordinators = [Coordinator]()
+//    var navigationController: UINavigationController
+//    
+//    init(navigationController: UINavigationController) {
+//        self.navigationController = navigationController
+//    }
+//    
+//    func start() {
+//        let vc = BuyViewController.instantiate()
+//        vc.coordinator = self
+//        navigationController.pushViewController(vc, animated: false)
+//    }
+//}
